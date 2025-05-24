7cfffb6f38b15683518614d9e480792b308631ca
Puneet Shrivas <puneetshrivas32@gmail.com>
2025-05-24 16:49:58 +0530

    changing path of streamlit application

--- email_agent/workflow.py
+++ email_agent/workflow.py
@@ -4,4 +4,4 @@ from agent.email_processor import process_email
 def handle_email(uid, mail):
     return process_email(uid, mail)
 def start_ui():
-    subprocess.Popen(["python", "-m", "streamlit", "run", "C:/Users/punee/OneDrive/Documents/GitHub/meeting_screener/email_agent/ui/streamlit_ui.py"])
+    subprocess.Popen(["python", "-m", "streamlit", "run", "ui/streamlit_ui.py"])
